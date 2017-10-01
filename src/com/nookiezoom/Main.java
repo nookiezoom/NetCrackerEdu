e0d68f8edb7424cb2b21141b871032256e17c0d3
Xarmor <nookiezoom@gmail.com>
2017-10-01 20:33:25 +0300

    Создан метод print

--- src/com/nookiezoom/Main.java
+++ src/com/nookiezoom/Main.java
@@ -1,8 +1,27 @@
+/**
+ *
+ * Solution for task #2
+ *
+ * @version 1.0 01 Oct 2017
+ * @author Svinarev Sergey
+ */
 package com.nookiezoom;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        print(args);
+
     }
+
+//очень вовремя вспомнил про конструкцию foreach
+    public static void print(String[] args) {
+        for (String str : args) {
+            System.out.println(str);
+        }
+    }
+
+
 }
+
