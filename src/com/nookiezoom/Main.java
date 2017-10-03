da8a9801097823bd7c180aa3154f8cb7e89cd316
Xarmor <nookiezoom@gmail.com>
2017-10-03 16:27:30 +0300

    Добавлен метод sort

--- src/com/nookiezoom/Main.java
+++ src/com/nookiezoom/Main.java
@@ -10,18 +10,38 @@ package com.nookiezoom;
 public class Main {
 
     public static void main(String[] args) {
-
+        print(args);
+        System.out.println();
+        args = sort(args);
         print(args);
 
     }
 
-//очень вовремя вспомнил про конструкцию foreach
+    //метод print
     public static void print(String[] args) {
+
+        //использование конструкции foreach
         for (String str : args) {
             System.out.println(str);
         }
     }
 
+    //метод sort
+    public static String[] sort(String[] args) {
+        int min;
+        for (int i = 0; i < args.length - 1; i++) {
+            min = i;
+            for (int j = args.length - 1; j > i; j--) {
+                if (args[min].compareTo(args[j]) > 0) {
+                    min = j;
+                }
+            }
+            String temp = args[min];
+            args[min] = args[i];
+            args[i] = temp;
+        }
+        return args;
+    }
 
 }
 
